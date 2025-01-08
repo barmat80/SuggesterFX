d18fe8aaa3b055ca911c09e177f7b710aec016b2
Mattia <mattia.bargellini@gmail.com>
2025-01-08 10:54:17 +0100

    Changed to abstract class

--- src/main/java/com/maemlab/suggesterfx/mvci/Model.java
+++ src/main/java/com/maemlab/suggesterfx/mvci/Model.java
@@ -1,4 +1,22 @@
 package com.maemlab.suggesterfx.mvci;
 
-public interface Model {
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+
+abstract class Model {
+    private final ObjectProperty<Throwable> error = new SimpleObjectProperty<>();
+    private final ObjectProperty<Boolean> deleteRequested = new SimpleObjectProperty<>(false);
+    private final ObjectProperty<Boolean> deleteConfirmed = new SimpleObjectProperty<>(false);
+
+    public ObjectProperty<Throwable> errorProperty() {
+        return error;
+    }
+
+    public ObjectProperty<Boolean> deleteRequestedProperty() {
+        return deleteRequested;
+    }
+
+    public ObjectProperty<Boolean> deleteConfirmedProperty() {
+        return deleteConfirmed;
+    }
 }
