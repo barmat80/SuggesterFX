b7dc78d4ccceed07550d0cbd0ec78913f539a775
Mattia <mattia.bargellini@gmail.com>
2025-05-17 15:18:12 +0200

    Module Info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module SuggesterFX {
+    requires javafx.controls;
+    requires com.maemlab.mvcifx;
+
+    exports com.maemlab.suggesterfx.entries;
+    exports com.maemlab.suggesterfx.mvci;
+    exports com.maemlab.suggesterfx.suggestion;
+    exports com.maemlab.suggesterfx.controlsfx.autocompletion.textfield;
+}
